7242c61cafed97b2ab1ead35ab28f1fc3c55f568
Anisha0906 <anishadas880@gmail.com>
2022-08-13 21:59:18 +0530

    Add:Ability to insert 30 between 56 and 70

--- LinkedList.cs
+++ LinkedList.cs
@@ -9,40 +9,69 @@ namespace LinkedList
     class LinkedList
     {
         internal Node head;
-       internal void Add(int data)
-        {
-          Node node = new Node(data);
-           if (this.head == null)
-                this.head = node;
-            else
-            {
-               Node temp = head;
-                while (temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            Console.WriteLine("{0} inserted into the linked list", node.data);
-        }
-        internal void Add2(int data)
+         internal void Add(int data)
         {
              Node node = new Node(data);
              if (this.head == null)
                 this.head = node;
             else
             {
-                node.next = head;
-                this.head = node;
+                 Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
             }
              Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+         internal void Add2(int data)
+        {
+            Node node = new Node(data);
+              if (this.head == null)
+                this.head = node;
+            else
+            {
+                 node.next = head;
+                this.head = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+         internal Node InsertAtParticularPosition(int position, int data)
+        {
+             if (position < 1)
+                 Console.WriteLine("Invalid position");
+            Console.WriteLine("Inserting " + data + " in positon " + position);
+             if (position == 1)
+            {
+                  var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+             {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
         }
          internal void Display()
         {
             Node temp = this.head;
              if (temp == null)
             {
-                Console.WriteLine("Linked list is empty");
+                 Console.WriteLine("Linked list is empty");
                 return;
             }
              Console.WriteLine("Sequence of LinkedList :-");
--- Program.cs
+++ Program.cs
@@ -20,6 +20,12 @@ namespace LinkedList
             list.Add2(30);
             list.Add2(56);
             list.Display();
+            list = new LinkedList();
+            list.Add(56);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 30);
+            list.Display();
+            Console.ReadLine();
             Console.ReadLine();
         }
     }
